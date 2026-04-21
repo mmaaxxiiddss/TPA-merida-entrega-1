ba2495452748c34c77d71dd72570d175b1f3a3b3
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-21 13:32:15 -0300

    Create Queja class with attributes
    Define the Queja class with relevant attributes.

---
+++ src/main/java/ar/edu/utn/dds/k3003/model/Queja.java
@@ -0,0 +1,11 @@
+package ar.edu.utn.dds.k3003.model;
+
+public class Queja {
+  
+  private String Id;
+  private String donacionId;
+  private String donadorId;
+  private LocalDate fecha;
+  private String descripcion;
+  
+  ]
