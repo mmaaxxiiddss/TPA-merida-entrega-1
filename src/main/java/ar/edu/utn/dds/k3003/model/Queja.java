7aa2452425f7892b99555e0c12914fc1c85b42fa
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-21 13:37:36 -0300

    Add getters and setters for Queja properties
    Added getter and setter methods for id, donacionId, and donadorId.

--- src/main/java/ar/edu/utn/dds/k3003/model/Queja.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Queja.java
@@ -9,3 +9,27 @@ public class Queja {
   private String descripcion;
   
   ]
+
+public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getDonacionId() {
+    return donacionId;
+  }
+
+  public void setDonacionId(String donacionId) {
+    this.donacionId = donacionId;
+  }
+
+  public String getDonadorId() {
+    return donadorId;
+  }
+
+  public void setDonadorId(String donadorId) {
+    this.donadorId = donadorId;
+  }
