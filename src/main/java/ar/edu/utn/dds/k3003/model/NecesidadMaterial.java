89d016217aa35227d054b060bacb1cfb7a42e182
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-21 14:33:54 -0300

    Create NecesidadMaterial class with attributes
    Added NecesidadMaterial class with properties and method.

---
+++ src/main/java/ar/edu/utn/dds/k3003/model/NecesidadMaterial.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.dds.k3003.model;
+
+public class NecesidadMaterial {
+  
+  private String entidadId;
+  private int nivelDeUrgencia;
+  private String descripcion;
+  private String productoSolicitadoID;
+  private int cantidadObjetivo;
+  private Enum tipoNecesidadEnum;
+  
+  ]
+
+public NecesidadMaterialDTO primerNecesidad(){
+
+
+  return null;
+}
