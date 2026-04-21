4ce34b0137659824bc636fbb5244762d50ac118b
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-21 13:33:56 -0300

    Create DonadoresRepository interface for Queja management

---
+++ src/main/java/ar/edu/utn/dds/k3003/repositories/QuejasRepository.java
@@ -0,0 +1,12 @@
+package ar.edu.utn.dds.k3003.repositories;
+
+import ar.edu.utn.dds.k3003.catedra.dtos.donaciones.QuejaDTO;
+import java.util.Optional;
+
+public interface DonadoresRepository {
+  Optional<Queja> findById(String id);
+
+  Queja save(Queja queja);
+
+ 
+}
