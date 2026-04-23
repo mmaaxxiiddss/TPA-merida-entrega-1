ae4665375cbb7b0cc470469a63e574b1de48b77d
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-23 12:41:09 -0300

    Deposito class with basic attributes
    Add Deposito class with attributes and getter/setter methods.

---
+++ src/main/java/ar/edu/utn/dds/k3003/model/Deposito.java
@@ -0,0 +1,26 @@
+package ar.edu.utn.dds.k3003.model;
+
+public class Deposito {
+  
+  private String nombre;
+  private String direccion;
+  private String capacidadMaxima;
+  private String stockActual;
+
+  ]
+
+public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String id) {
+    this.nombre = nombre;
+  }
+
+  public String getDonacionId() {
+    return donacionId;
+  }
+
+  public void setDonacionId(String donacionId) {
+    this.donacionId = donacionId;
+  }
