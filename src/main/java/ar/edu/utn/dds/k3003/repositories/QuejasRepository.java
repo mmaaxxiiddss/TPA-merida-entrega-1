5faaa0efbfebfbaf90a04da4f61fcd0890dd4bce
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-23 12:31:46 -0300

    Fix DonadoresRepository to QuejasRepository

--- src/main/java/ar/edu/utn/dds/k3003/repositories/QuejasRepository.java
+++ src/main/java/ar/edu/utn/dds/k3003/repositories/QuejasRepository.java
@@ -3,7 +3,7 @@ package ar.edu.utn.dds.k3003.repositories;
 import ar.edu.utn.dds.k3003.catedra.dtos.donaciones.QuejaDTO;
 import java.util.Optional;
 
-public interface DonadoresRepository {
+public interface QuejasRepository {
   Optional<Queja> findById(String id);
 
   Queja save(Queja queja);
