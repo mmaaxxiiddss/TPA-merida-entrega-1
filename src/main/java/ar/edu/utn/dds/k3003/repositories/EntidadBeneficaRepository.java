991f0537f4308be682477bba49691b37152f029d
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-23 18:57:59 -0300

    EntidadBeneficaRepository

--- src/main/java/ar/edu/utn/dds/k3003/repositories/EntidadBeneficaRepository.java
+++ src/main/java/ar/edu/utn/dds/k3003/repositories/EntidadBeneficaRepository.java
@@ -1,9 +1,9 @@
 package ar.edu.utn.dds.k3003.repositories;
 
-import ar.edu.utn.dds.k3003.model.NecesidadMaterial;
+import ar.edu.utn.dds.k3003.model.EntidadBenefica;
 import java.util.Optional;
 
-public interface NecesidadMsterialRepository {
+public interface EntidadBeneficaRepository {
   Optional<EntidadBeneficaRepository> findById(String id);
 
   EntidadBeneficaRepository save(EntidadBenefica entidadBenefica);
