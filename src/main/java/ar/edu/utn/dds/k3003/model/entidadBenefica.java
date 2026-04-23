2cfe570eafe4b45a7617953ba5167e08c0385fba
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-23 12:48:53 -0300

    entidadBenefica class with attributes and methods
    Added fields and getter/setter for entidadBenefica class.

---
+++ src/main/java/ar/edu/utn/dds/k3003/model/entidadBenefica.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.dds.k3003.model;
+
+public class entidadBenefica {
+  
+  private String Id;
+  private String razonSocial;
+  private String domicilio;
+  private String telefono;
+  private String correoElectronico;
+
+}
+
+public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
