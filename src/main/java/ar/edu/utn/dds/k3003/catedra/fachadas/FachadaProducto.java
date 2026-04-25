9df5e1d71fa77332130bd3cf99f1320dddf972c9
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-04-25 15:40:22 -0300

    agregarProducto method to ProductoDTO

--- src/main/java/ar/edu/utn/dds/k3003/catedra/fachadas/FachadaProducto.java
+++ src/main/java/ar/edu/utn/dds/k3003/catedra/fachadas/FachadaProducto.java
@@ -11,7 +11,7 @@ import java.util.NoSuchElementException;
 
 public interface FachadaProducto {
 
-  ProductoDTO agregarProducto();
+  ProductoDTO agregarProducto(ProductoDTO productoDTO);
   
   ProductoDTO registrarProducto(ProductoDTO productoDTO);
 
