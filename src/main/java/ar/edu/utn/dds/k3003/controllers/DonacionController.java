6447acbdce0d58fbc3451686f50283fe86e96eb3
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-05-12 19:13:46 -0300

    Add fachada

--- src/main/java/ar/edu/utn/dds/k3003/controllers/DonacionController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controllers/DonacionController.java
@@ -1,9 +1,23 @@
 package ar.edu.utn.dds.k3003.controllers;
 
+private Fachada fachada;
+
+
 public class DonacionController{
 
+    public List<QuejasDTO> obtenerQuejasDe(DonadorDTO donadorDTO){
+  
+    val listaDeQuejas = this.fachada.quejaRepository.findById(donadorDTO.getId());
+    List<Queja> lista = new ArrayList<>();
+        for(val que -> listaDeQuejas){
+            lista.add(this.fachada.quejaDataMapper.toQuejaDTO(que));
+        }
+    return lista;
+  
+    }
 
-    public 
 
+
+    
   
 }
