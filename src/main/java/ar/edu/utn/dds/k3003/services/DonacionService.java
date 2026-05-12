603099f0f0e8bb943dcca7abbc7f411347d5e543
maxi merida 2025 <168112355+mmaaxxiiddss@users.noreply.github.com>
2026-05-12 19:00:22 -0300

    Add fachada

--- src/main/java/ar/edu/utn/dds/k3003/services/DonacionService.java
+++ src/main/java/ar/edu/utn/dds/k3003/services/DonacionService.java
@@ -8,24 +8,36 @@ public class DonacionService()
 {
 
 public List<QuejasDTO> obtenerQuejasDe(DonadorDTO donadorDTO){
-  val listaDeQuejas = this.donacionController.obtenerQuejasDe(donadorDTO.Id);
-return null;
-}
-
-public NecesidadMaterialDTO registratNecesidadMaterial(NecesidadMaterialDTO){
   
-  val necesidadMaterial = this.donacionController.registrarNecesidadMaterial(necesidadMaterialDTO)
-  return necesidadMaterial;
+val listaDeQuejas = this.donacionController.obtenerQuejasDe(donadorDTO);
+return listaDeQuejas;
+  
+}
+
+public NecesidadMaterialDTO registrarNecesidadMaterial(NecesidadMaterialDTO){
+   
+  return this.donacionController.registrarNecesidadMaterial(necesidadMaterialDTO);
 
 }
 
-public NecesidadMaterialDTO guardarNecesidadMaterial(NecesidadMaterialDTO necesidadMaterial){
-  val necesidadMaterial = this.donacionController.guardarNecesidadMaterial(necesidadMaterialDTO)
-  return necesidadMaterial;
-  }
+public EntidadBeneficaDTO guardarNecesidadMaterial(EntidadBeneficaDTO entidadBeneficaDTO,NecesidadMaterialDTO necesidadMaterialDTO){
+  
+  return this.donacionController.guardarNecesidadMaterial(entidadBeneficaDTO,NecesidadMaterialDTO);
 
-public EntidadBeneficaDTO buscarEntidadPorId(){
-    return this.donacionController.buscarEntidadPorId();
+}
+
+public EntidadBeneficaDTO buscarEntidadPorId(String id){
     
-  }
+  return this.donacionController.buscarEntidadPorId(id);
+    
 }
+
+
+
+
+}
+
+
+
+
+
